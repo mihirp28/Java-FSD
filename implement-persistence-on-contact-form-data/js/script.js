8f5626630ba7954a77f38d941e819b8a8174d8d8
Mihir Patel <mihirjpatel282000@gmail.com>
2024-06-02 11:16:25 -0400

    practice

---
+++ implement-persistence-on-contact-form-data/js/script.js
@@ -0,0 +1,16 @@
+// provide the validation code here
+
+function submitContact(event) {
+    console.log("inside submitContact ",event);
+    const form=event.target;
+    const formData=new FormData(form);
+    console.log("form entries",formData.entries());
+    const contact={};
+    for(let entry of formData.entries()){
+        console.log(entry[0],entry[1]);
+        contact[entry[0]]=entry[1];
+    }
+    persistSubmittedContact(contact);
+    
+}
+
