f10051e64e70cf987164f23a1012bc75ba68bf52
Mihir Patel <mihirjpatel282000@gmail.com>
2024-06-11 23:22:14 -0400

    practice

---
+++ newz-step6-boilerplate/newz-netflix-eureka-server/src/test/java/com/stackroute/NewzNetflixEurekaServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.stackroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.stackroute.newz.NewzNetflixEurekaServerApplication;
+
+@SpringBootTest(classes = NewzNetflixEurekaServerApplication.class)
+class NewzNetflixEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ newz-step6-boilerplate/newz-netflix-zuul-api-gateway/src/test/java/com/stackroute/NewzNetflixZuulApiGatwayApplicationTests.java
@@ -0,0 +1,15 @@
+package com.stackroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.stackroute.newz.NewzNetflixZuulApiGatwayApplication;
+
+@SpringBootTest(classes = NewzNetflixZuulApiGatwayApplication.class)
+class NewzNetflixZuulApiGatwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
