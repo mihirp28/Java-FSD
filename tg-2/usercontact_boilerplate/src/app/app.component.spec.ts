a04c4858767353e6f738d7ba5c43b09a38b2919e
Mihir Patel <mihirjpatel282000@gmail.com>
2024-06-12 20:44:27 -0400

    practice

---
+++ tg-2/usercontact_boilerplate/src/app/app.component.spec.ts
@@ -0,0 +1,37 @@
+import { TestBed, waitForAsync } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+
+
+describe('AppComponent', () => {
+
+  let component: AppComponent;
+  beforeEach(waitForAsync(() => {
+
+    TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        HttpClientModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'contactform'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('Angular Contact Form');
+  });
+
+});
