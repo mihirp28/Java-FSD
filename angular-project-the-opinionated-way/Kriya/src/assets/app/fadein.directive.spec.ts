19f51c43f85b2d5a2f489c84b8f5b6ba5b83e85d
Mihir Patel <mihirjpatel282000@gmail.com>
2024-07-05 12:19:06 -0400

    practice

--- angular-project-the-opinionated-way/Kriya/src/assets/app/fadein.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FadeinDirective } from './fadein.directive';
-
-describe('FadeinDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FadeinDirective();
-    expect(directive).toBeTruthy();
-  });
-});
