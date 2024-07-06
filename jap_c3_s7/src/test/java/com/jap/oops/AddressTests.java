8f7ff991159a8b9d837414063294c6500e804add
Mihir Patel <mihirjpatel282000@gmail.com>
2024-07-06 15:31:15 -0400

    practice

---
+++ jap_c3_s7/src/test/java/com/jap/oops/AddressTests.java
@@ -0,0 +1,49 @@
+package com.jap.oops;
+
+import com.jap.oops.Address;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class AddressTests {
+    private Address addressOne;
+    private Address addressTwo;
+
+    @BeforeEach
+    public void setUp() {
+        addressOne = new Address("HSR", "Bangalore");
+        addressTwo = new Address("HSR", "Bangalore");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        addressOne = null;
+        addressTwo = null;
+    }
+
+    @Test
+    public void givenValidPropertyValuesWhenAddressObjectCreatedThenPropertiesAreSet() {
+        assertEquals("HSR", addressOne.getArea());
+        assertEquals("Bangalore", addressOne.getCity());
+    }
+
+    @Test
+    public void givenTwoAddressObjectsWhenPropertyValuesAreSameThenEqualsMethodReturnsTrue() {
+        assertTrue(addressOne.equals(addressTwo));
+        assertTrue(addressTwo.equals(addressOne));
+    }
+
+
+
+    @Test
+    public void givenTwoAddressObjectsWhenBothObjectsPropertyValueIsNullThenEqualsMethodReturnsTrue() {
+        addressOne.setCity(null);
+        addressTwo.setCity(null);
+        assertTrue(addressOne.equals(addressTwo));
+        assertTrue(addressTwo.equals(addressOne));
+    }
+
+}
