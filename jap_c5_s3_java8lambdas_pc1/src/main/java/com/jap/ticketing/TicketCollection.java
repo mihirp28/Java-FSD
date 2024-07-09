ea2b32a06bcb3eca03dcd38bad679252e9807bd4
Mihir Patel <mihirjpatel282000@gmail.com>
2024-07-09 18:59:26 -0400

    practice

---
+++ jap_c5_s3_java8lambdas_pc1/src/main/java/com/jap/ticketing/TicketCollection.java
@@ -0,0 +1,7 @@
+package com.jap.ticketing;
+import java.util.List;
+public interface TicketCollection {
+   // define the abstract method for calculating total collection amount
+     public double totalAmountCollected(List<TicketingData> ticketingDataList);
+
+}
