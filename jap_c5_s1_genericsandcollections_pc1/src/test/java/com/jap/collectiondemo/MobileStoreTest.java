766babd788b829dcd97c8e7c259fc34c693ae5d4
Mihir Patel <mihirjpatel282000@gmail.com>
2024-07-09 18:58:51 -0400

    practice

---
+++ jap_c5_s1_genericsandcollections_pc1/src/test/java/com/jap/collectiondemo/MobileStoreTest.java
@@ -0,0 +1,59 @@
+package com.jap.collectiondemo;
+
+import com.jap.collectiondemo.Mobile;
+import com.jap.collectiondemo.MobileStore;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MobileStoreTest {
+
+     MobileStore mobileStore;
+
+     @Test
+    public void givenInputSamsumgShouldReturnAllSamsungMobile()
+    {
+        mobileStore = new MobileStore();
+        mobileStore.readMobileData("input.txt");
+        List brand = mobileStore.findPhoneByBrand("Samsung");
+        assertEquals(4,brand.size());
+
+    }
+    @Test
+    public void givenInputAppleShouldReturnAllAppleMobile()
+    {
+        mobileStore = new MobileStore();
+        mobileStore.readMobileData("input.txt");
+        List brand = mobileStore.findPhoneByBrand("Apple");
+        assertEquals(3,brand.size());
+
+    }
+    @Test
+    public void givenInputCostMoreThan500ShouldReturnMobile()
+    {
+        mobileStore = new MobileStore();
+        mobileStore.readMobileData("input.txt");
+        List brand = mobileStore.findPhoneCostMoreThan500$();
+        assertEquals(3,brand.size());
+
+    }
+    @Test
+    public void givenInputPixelMoreThan12ShouldReturnMobile()
+    {
+        mobileStore = new MobileStore();
+        mobileStore.readMobileData("input.txt");
+        List brand = mobileStore.findPhonePixelMoreThan12MP();
+        assertEquals(5,brand.size());
+
+    }
+
+
+
+
+}
